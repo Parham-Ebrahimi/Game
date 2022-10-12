a3710c8f70c514817c19b5a148ae9d3c7d5b1b4b
Parham-Ebrahimi <parham.ebrahimi123@gmail.com>
2022-10-11 21:27:02 -0500

    fixed colliders and tilemaps
    Made it so if the character is behind an object then it shows him behind the object rather than what it did before which was that it was on top of the object

--- Game/Assets/playerMovement.cs
+++ Game/Assets/playerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-
+    public GameObject player;
     public Vector2 playerInput;
     public Rigidbody2D rb;
 
@@ -30,5 +30,40 @@ public class playerMovement : MonoBehaviour
         {
             animator.SetBool("moving",false);
         }
+        GameObject obj = FindClosestObject();
+        GameObject player = GameObject.Find("MainCharacter");
+        //obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y, 0);;
+        if(player.transform.position.y > obj.transform.position.y)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sortingOrder = 1;
+            //transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+            Debug.Log(0);
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().sortingOrder = 3;
+            //transform.position = new Vector3(transform.position.x, transform.position.y, 10);
+            Debug.Log(2);
+        }
+        Debug.Log(obj.name);
+    }
+    public GameObject FindClosestObject()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Overlapping");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
     }
 }
